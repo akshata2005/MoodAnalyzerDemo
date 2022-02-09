8bfe920d12e7050568911e453293371f1bda3568
akshata2005 <akshatasawant1805@gmail.com>
2022-02-09 20:19:42 +0530

    [Akshata] Add: change mood dynamically

--- MoodAnalyzer1/MoodAnalyzer1/MoodAnalyzerException.cs
+++ MoodAnalyzer1/MoodAnalyzer1/MoodAnalyzerException.cs
@@ -14,7 +14,7 @@ namespace MoodAnalyzer1
         public enum ExceptionType
         {
             NULL_EXCEPTION, EMPTY_EXCEPTION,
-            CLASS_NOT_FOUND ,CONSTRUCTOR_NOT_FOUND,NO_SUCH_METHOD
+            CLASS_NOT_FOUND ,CONSTRUCTOR_NOT_FOUND,NO_SUCH_METHOD, NULL_MOOD, NO_SUCH_FIELD
         }
         public MoodAnalyzerException(ExceptionType type, string message) : base(message)  //parameterized constructor and also using bsae method
         {
