9220f89edfc96cbfef70442cc1422e3e78d93a8c
akshata2005 <akshatasawant1805@gmail.com>
2022-02-09 20:04:38 +0530

    [Akshata] Add: Refactor Mood Analyzer Reflector

--- MoodAnalyzer1/MoodAnalyzer1/MoodAnalyzerException.cs
+++ MoodAnalyzer1/MoodAnalyzer1/MoodAnalyzerException.cs
@@ -14,7 +14,7 @@ namespace MoodAnalyzer1
         public enum ExceptionType
         {
             NULL_EXCEPTION, EMPTY_EXCEPTION,
-            CLASS_NOT_FOUND ,CONSTRUCTOR_NOT_FOUND
+            CLASS_NOT_FOUND ,CONSTRUCTOR_NOT_FOUND,NO_SUCH_METHOD
         }
         public MoodAnalyzerException(ExceptionType type, string message) : base(message)  //parameterized constructor and also using bsae method
         {
